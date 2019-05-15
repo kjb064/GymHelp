5c3cc8fb79c700163971cabd6d972568d9e395e8
kjb064 <kxbarry@yahoo.com>
2019-05-15 00:01:15 -0500

    New constants file for use with identifying the categories of the exercises based on their targets.

---
+++ app/src/main/java/com/example/android/gymhelp/Constants.java
@@ -0,0 +1,10 @@
+package com.example.android.gymhelp;
+
+public class Constants {
+    public static final int CHEST = 0;
+    public static final int LEGS = 1;
+    public static final int BACK = 2;
+    public static final int SHOULDERS = 3;
+    public static final int ARMS = 4;
+    public static final int ABS = 5;
+}
