c3c183ea4f849f70f6a88d4e81022ccf90e21be4
kjb064 <kxbarry@yahoo.com>
2019-08-14 17:06:36 -0500

    Added refreshSearchResults method to refresh the listView when the data has been modified. The listView now uses the ExerciseClickListener class as its onItemClickListener.

--- app/src/main/java/com/example/android/gymhelp/SearchResultsActivity.java
+++ app/src/main/java/com/example/android/gymhelp/SearchResultsActivity.java
@@ -7,14 +7,16 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class SearchResultsActivity extends AppCompatActivity {
 
+    // TODO: Implement search suggestions
+
     private ExerciseAdapter adapter;
     private ListView listView;
+    private String query;
+    private ArrayList<Exercise> exercises;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -22,11 +24,11 @@ public class SearchResultsActivity extends AppCompatActivity {
         setContentView(R.layout.exercise_list);
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
+            query = intent.getStringExtra(SearchManager.QUERY);
 
             //use the query to search
             DatabaseHelper db = new DatabaseHelper(this);
-            ArrayList<Exercise> exercises = db.getQueryResults(query);
+            exercises = db.getQueryResults(query);
 
             adapter = new ExerciseAdapter(this, exercises, R.color.title_color);
             listView = (ListView) findViewById(R.id.list);
@@ -34,12 +36,25 @@ public class SearchResultsActivity extends AppCompatActivity {
             // registerForContextMenu(listView);
 
             listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new ExerciseClickListener(listView, this, Constants.NO_FRAGMENT_ID));
 
             // Notify if no results??
 
-            // TODO: Possibly abstract the onClickListener for the list items into a class so it can be used here
 
 
         }
-    }
+    } // end onCreate
+
+    /*
+    * Called when the ListView of this Activity needs to be updated to reflect a change in the data
+    * displayed.
+     */
+    public void refreshSearchResults(){
+        DatabaseHelper db = new DatabaseHelper(this);
+        exercises = db.getQueryResults(query);
+        adapter.clear();
+        adapter.addAll(exercises);
+        adapter.notifyDataSetChanged();
+        listView.invalidate();
+    } // end refreshSearchResults
 }
