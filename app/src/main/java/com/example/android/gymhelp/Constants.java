6d6d08365ce4e6e5159597713e09f2ba03f3359c
kjb064 <kxbarry@yahoo.com>
2019-08-18 00:49:53 -0500

    Added new Compound variable. Changed num targets from 6 to 7. Moved menu related vars here.

--- app/src/main/java/com/example/android/gymhelp/Constants.java
+++ app/src/main/java/com/example/android/gymhelp/Constants.java
@@ -1,5 +1,7 @@
 package com.example.android.gymhelp;
 
+import android.view.Menu;
+
 public class Constants {
     public static final int CHEST = 0;
     public static final int LEGS = 1;
@@ -7,9 +9,14 @@ public class Constants {
     public static final int SHOULDERS = 3;
     public static final int ARMS = 4;
     public static final int ABS = 5;
+    public static final int COMPOUND = 6;
 
-    public static final int NUM_TARGETS = 6;
+    public static final int NUM_TARGETS = 7;
     public static final String NO_IMAGE_PROVIDED = "NONE";
     public static final String NO_FRAGMENT_ID = "NO_ID";
     public static final String DEFAULT_DATE = "--";
+
+    public static final int READ_FULL_ID = Menu.FIRST;
+    public static final int DELETE_ID = Menu.FIRST + 1;
+    public static final int EDIT_ID = Menu.FIRST + 2;
 }
