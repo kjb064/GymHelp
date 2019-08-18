2d0e69de517c2d150af227ef0bf8a3137bf29ce6
kjb064 <kxbarry@yahoo.com>
2019-08-18 00:38:54 -0500

    Changed TABLE_NAME string to "defaultWorkout" from "cutting". Added getCompoundExercises method. Added two new methods to build the defaultWorkout table with different data.

--- app/src/main/java/com/example/android/gymhelp/DatabaseHelper.java
+++ app/src/main/java/com/example/android/gymhelp/DatabaseHelper.java
@@ -13,8 +13,13 @@ import java.util.Calendar;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
+    // TODO:
+    // Check for duplicates when inserting new exercise (do this before passing to DB helper...?)
+    // Consolidate "getExercise" methods into one?
+    // Consolidate the last part of the createDefault___Table where exercises are added into one method?
+
     private static final String DATABASE_NAME = "GymHelper.db";
-    private static final String TABLE_NAME = "cutting";
+    private static final String TABLE_NAME = "defaultWorkout";
     private static final String ID = "ID";
     private static final String EXERCISE_NAME = "name";
     private static final String WEIGHT = "weight";
@@ -28,7 +33,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     public DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, 19);     // Most recent version: 19
+        super(context, DATABASE_NAME, null, 20);     // Most recent version: 20
         this.context = context;
     }
 
@@ -43,169 +48,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + IMAGE_PATH + " TEXT, "
                 + EXERCISE_TARGET + " INTEGER" + ")" );
 
-        ContentValues values = new ContentValues();
-        final ArrayList<Exercise> exercises = new ArrayList<Exercise>();
-
-        /*
-         * Day 1: Chest
-         */
-        exercises.add(new Exercise("Barbell Bench Press - Medium Grip",
-                "5 sets, 15, 12, 10, 10, 10 reps ",
-                0,
-                Constants.CHEST) );
-        exercises.add(new Exercise("Incline Dumbbell Press",
-                "4 sets, 12, 10, 10, 8 reps ",
-                0,
-                Constants.CHEST) );
-        exercises.add(new Exercise("Dumbbell Flyes",
-                "4 sets, 10, 10, 10, 10 reps",
-                0,
-                Constants.CHEST));
-        exercises.add(new Exercise("Straight-Arm Dumbbell Pullover",
-                "3 sets, 15, 12, 10 reps ",
-                0,
-                Constants.CHEST) );
-        exercises.add(new Exercise("Butterfly",
-                "4 sets, 12, 12, 12, 12 reps",
-                0,
-                Constants.CHEST) );
-
-        /*
-         * Day 2: Quads/Calves
-         *  &
-         * Day 6: Calves/Hamstrings
-         */
-        exercises.add(new Exercise("Standing Calf Raises",
-                "3 sets, 60, 60, 60 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Seated Calf Raise",
-                "3 sets, 60, 60, 60 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Leg Extensions",
-                "5 sets, 15, 12, 12, 10, 10 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Barbell Squat",
-                "5 sets, 20, 15, 12, 10, 10 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Leg Press",
-                "4 sets, 15, 12, 12, 10 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Smith Machine Squat",
-                "3 sets, 15, 15, 15 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Seated Leg Curl",
-                "4 sets, 12, 10, 10, 10 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Stiff-Legged Barbell Deadlift",
-                "4 sets, 15, 12, 12, 10 reps",
-                0,
-                Constants.LEGS) );
-        exercises.add(new Exercise("Dumbbell Lunges",
-                "3 sets, 20 steps",
-                0,
-                Constants.LEGS) );
-
-        /*
-         * Day 3: Back
-         */
-        exercises.add(new Exercise("Wide-Grip Lat Pulldown",
-                "4 sets, 12, 10, 10, 10 reps",
-                0,
-                Constants.BACK) );
-        exercises.add(new Exercise("Seated Cable Rows",
-                "4 sets, 15, 12, 10, 10 reps",
-                0,
-                Constants.BACK) );
-        exercises.add(new Exercise("Bent Over Barbell Row",
-                "4 sets, 15, 12, 10, 8 reps",
-                0,
-                Constants.BACK) );
-        exercises.add(new Exercise("One-Arm Dumbbell Row",
-                "4 sets, 15, 10, 10, 8 reps",
-                0,
-                Constants.BACK) );
-
-
-        /*
-         * Day 4: Shoulders
-         */
-        exercises.add(new Exercise("Standing Military Press",
-                "4 sets, 12, 10, 8, 8 reps",
-                0,
-                Constants.SHOULDERS) );
-        exercises.add(new Exercise("Dumbbell Bench Press",
-                "4 sets, 10, 10, 8, 8 reps",
-                0,
-                Constants.SHOULDERS) );
-        exercises.add(new Exercise("Barbell Shrug",
-                "4 sets, 15, 12, 12, 10 reps",
-                0,
-                Constants.SHOULDERS) );
-        exercises.add(new Exercise("Smith Machine Shrug",
-                "3 sets, 12, 12, 12 reps",
-                0,
-                Constants.SHOULDERS) );
-        exercises.add(new Exercise("Side Lateral Raise",
-                "3 sets, 12, 10, 8 reps per side",
-                0,
-                Constants.SHOULDERS) );
-        exercises.add(new Exercise("Front Plate Raise",
-                "3 sets, 12, 10, 8 reps (25, 35, 45)",
-                0,
-                Constants.SHOULDERS) );
-
-
-        /*
-         * Day 5: Arms
-         */
-        exercises.add(new Exercise("Barbell Curl",
-                "4 sets, 12, 10, 10, 8 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Dumbbell Alternate Bicep Curl",
-                "4 sets, 12, 10, 8, 8 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Standing Dumbbell Reverse Curl",
-                "4 sets, 12, 10, 10, 8 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("One Arm Dumbbell Preacher Curl",
-                "3 sets, 12, 12, 12 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Dumbbell One-Arm Triceps Extension",
-                "4 sets, 12, 10, 10, 8 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Weighted Bench Dip",
-                "4 sets, 15, 12, 12, 10 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Lying Triceps Press",
-                "4 sets, 15, 10, 10, 8 reps",
-                0,
-                Constants.ARMS) );
-        exercises.add(new Exercise("Triceps Pushdown",
-                "3 sets, 12, 12, 10 reps",
-                0,
-                Constants.ARMS) );
-
-        for(int i = 0; i < exercises.size(); i++){
-            values.put(EXERCISE_NAME, exercises.get(i).getExerciseName());
-            values.put(WEIGHT, exercises.get(i).getRecentWeight());
-            values.put(SETS_REPS, exercises.get(i).getSetsAndReps());
-            values.put(DATE, Constants.DEFAULT_DATE);
-            values.put(EXERCISE_TARGET, exercises.get(i).getExerciseTarget());
-            db.insert(TABLE_NAME, null, values);
-        }
+        createDefaultPPLTable(db);
 
     } // end onCreate
 
@@ -388,6 +231,27 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return exercises;
     } // end getAbsExercises
 
+    public ArrayList getCompoundExercises(){
+        ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + TABLE_NAME +
+                " WHERE " + EXERCISE_TARGET + " = " + Constants.COMPOUND + ";", null);
+        if(c.moveToFirst()){
+            do{
+                int id = c.getInt(0);
+                String name = c.getString(1);
+                float weight = c.getFloat(2);
+                String sets = c.getString(3);
+                String date = c.getString(4);
+                String imagePath = c.getString(5);
+                exercises.add(new Exercise(id, name, sets, weight, imagePath, date));
+            }while(c.moveToNext());
+        }
+
+        c.close();
+        return exercises;
+    } // end getCompoundExercises
+
     public void updateExerciseWeight(int exerciseID, float weight){
         SQLiteDatabase db = this.getWritableDatabase();
 
@@ -537,4 +401,281 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
     } // end updateExercise
+
+    private void createDefaultPPLTable(SQLiteDatabase db){
+        ContentValues values = new ContentValues();
+        final ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+
+        /*
+         * Pull
+         */
+        exercises.add(new Exercise("Deadlifts",
+                "1x5+",
+                0,
+                Constants.COMPOUND));
+        exercises.add(new Exercise("Barbell Rows",
+                "4x5, 1x5+",
+                0,
+                Constants.BACK));
+        exercises.add(new Exercise("Pulldowns",
+                "3x8-12",
+                0,
+                Constants.BACK));
+        exercises.add(new Exercise("Pullups",
+                "3x8-12",
+                0,
+                Constants.COMPOUND));
+        exercises.add(new Exercise("Chin-ups",
+                "3x8-12",
+                0,
+                Constants.COMPOUND));
+        exercises.add(new Exercise("Seated Cable Rows",
+                "3x8-12",
+                0,
+                Constants.BACK));
+        exercises.add(new Exercise("Chest Supported Rows",
+                "3x8-12",
+                0,
+                Constants.BACK));
+        exercises.add(new Exercise("Face Pulls",
+                "5x15-20",
+                0,
+                Constants.SHOULDERS));
+        exercises.add(new Exercise("Hammer Curls",
+                "4x8-12",
+                0,
+                Constants.ARMS));
+        exercises.add(new Exercise("Dumbbell Curls",
+                "4x8-12",
+                0,
+                Constants.ARMS));
+
+        /*
+         * Push
+         */
+        exercises.add(new Exercise("Bench Press",
+                "4x5, 1x5+ OR 3x8-12",
+                0,
+                Constants.CHEST));
+        exercises.add(new Exercise("Overhead Press",
+                "4x5, 1x5+ OR 3x8-12",
+                0,
+                Constants.COMPOUND));
+        exercises.add(new Exercise("Incline Dumbbell Press",
+                "3x8-12",
+                0,
+                Constants.CHEST));
+        exercises.add(new Exercise("Triceps Pushdowns",
+                "3x8-12, SS Lateral Raises",
+                0,
+                Constants.ARMS));
+        exercises.add(new Exercise("Lateral Raises",
+                "3x15-20",
+                0,
+                Constants.SHOULDERS));
+        exercises.add(new Exercise("Overhead Triceps Extensions",
+                "3x8-12, SS Lateral Raises",
+                0,
+                Constants.ARMS));
+
+        /*
+         * Legs
+         */
+        exercises.add(new Exercise("Squats",
+                "2x5, 1x5+",
+                0,
+                Constants.LEGS));
+        exercises.add(new Exercise("Romanian Deadlifts",
+                "3x8-12",
+                0,
+                Constants.COMPOUND));
+        exercises.add(new Exercise("Leg Press",
+                "3x8-12",
+                0,
+                Constants.LEGS));
+        exercises.add(new Exercise("Leg Curls",
+                "3x8-12",
+                0,
+                Constants.LEGS));
+        exercises.add(new Exercise("Calf Raises",
+                "5x8-12",
+                0,
+                Constants.LEGS));
+
+
+        for(int i = 0; i < exercises.size(); i++){
+            values.put(EXERCISE_NAME, exercises.get(i).getExerciseName());
+            values.put(WEIGHT, exercises.get(i).getRecentWeight());
+            values.put(SETS_REPS, exercises.get(i).getSetsAndReps());
+            values.put(DATE, Constants.DEFAULT_DATE);
+            values.put(EXERCISE_TARGET, exercises.get(i).getExerciseTarget());
+            db.insert(TABLE_NAME, null, values);
+        }
+    } // end createDefaultPPLTable
+
+    private void createDefaultCuttingTable(SQLiteDatabase db){
+        ContentValues values = new ContentValues();
+        final ArrayList<Exercise> exercises = new ArrayList<Exercise>();
+
+        /*
+         * Day 1: Chest
+         */
+        exercises.add(new Exercise("Barbell Bench Press - Medium Grip",
+                "5 sets, 15, 12, 10, 10, 10 reps ",
+                0,
+                Constants.CHEST) );
+        exercises.add(new Exercise("Incline Dumbbell Press",
+                "4 sets, 12, 10, 10, 8 reps ",
+                0,
+                Constants.CHEST) );
+        exercises.add(new Exercise("Dumbbell Flyes",
+                "4 sets, 10, 10, 10, 10 reps",
+                0,
+                Constants.CHEST));
+        exercises.add(new Exercise("Straight-Arm Dumbbell Pullover",
+                "3 sets, 15, 12, 10 reps ",
+                0,
+                Constants.CHEST) );
+        exercises.add(new Exercise("Butterfly",
+                "4 sets, 12, 12, 12, 12 reps",
+                0,
+                Constants.CHEST) );
+
+        /*
+         * Day 2: Quads/Calves
+         *  &
+         * Day 6: Calves/Hamstrings
+         */
+        exercises.add(new Exercise("Standing Calf Raises",
+                "3 sets, 60, 60, 60 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Seated Calf Raise",
+                "3 sets, 60, 60, 60 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Leg Extensions",
+                "5 sets, 15, 12, 12, 10, 10 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Barbell Squat",
+                "5 sets, 20, 15, 12, 10, 10 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Leg Press",
+                "4 sets, 15, 12, 12, 10 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Smith Machine Squat",
+                "3 sets, 15, 15, 15 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Seated Leg Curl",
+                "4 sets, 12, 10, 10, 10 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Stiff-Legged Barbell Deadlift",
+                "4 sets, 15, 12, 12, 10 reps",
+                0,
+                Constants.LEGS) );
+        exercises.add(new Exercise("Dumbbell Lunges",
+                "3 sets, 20 steps",
+                0,
+                Constants.LEGS) );
+
+        /*
+         * Day 3: Back
+         */
+        exercises.add(new Exercise("Wide-Grip Lat Pulldown",
+                "4 sets, 12, 10, 10, 10 reps",
+                0,
+                Constants.BACK) );
+        exercises.add(new Exercise("Seated Cable Rows",
+                "4 sets, 15, 12, 10, 10 reps",
+                0,
+                Constants.BACK) );
+        exercises.add(new Exercise("Bent Over Barbell Row",
+                "4 sets, 15, 12, 10, 8 reps",
+                0,
+                Constants.BACK) );
+        exercises.add(new Exercise("One-Arm Dumbbell Row",
+                "4 sets, 15, 10, 10, 8 reps",
+                0,
+                Constants.BACK) );
+
+
+        /*
+         * Day 4: Shoulders
+         */
+        exercises.add(new Exercise("Standing Military Press",
+                "4 sets, 12, 10, 8, 8 reps",
+                0,
+                Constants.SHOULDERS) );
+        exercises.add(new Exercise("Dumbbell Bench Press",
+                "4 sets, 10, 10, 8, 8 reps",
+                0,
+                Constants.SHOULDERS) );
+        exercises.add(new Exercise("Barbell Shrug",
+                "4 sets, 15, 12, 12, 10 reps",
+                0,
+                Constants.SHOULDERS) );
+        exercises.add(new Exercise("Smith Machine Shrug",
+                "3 sets, 12, 12, 12 reps",
+                0,
+                Constants.SHOULDERS) );
+        exercises.add(new Exercise("Side Lateral Raise",
+                "3 sets, 12, 10, 8 reps per side",
+                0,
+                Constants.SHOULDERS) );
+        exercises.add(new Exercise("Front Plate Raise",
+                "3 sets, 12, 10, 8 reps (25, 35, 45)",
+                0,
+                Constants.SHOULDERS) );
+
+
+        /*
+         * Day 5: Arms
+         */
+        exercises.add(new Exercise("Barbell Curl",
+                "4 sets, 12, 10, 10, 8 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Dumbbell Alternate Bicep Curl",
+                "4 sets, 12, 10, 8, 8 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Standing Dumbbell Reverse Curl",
+                "4 sets, 12, 10, 10, 8 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("One Arm Dumbbell Preacher Curl",
+                "3 sets, 12, 12, 12 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Dumbbell One-Arm Triceps Extension",
+                "4 sets, 12, 10, 10, 8 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Weighted Bench Dip",
+                "4 sets, 15, 12, 12, 10 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Lying Triceps Press",
+                "4 sets, 15, 10, 10, 8 reps",
+                0,
+                Constants.ARMS) );
+        exercises.add(new Exercise("Triceps Pushdown",
+                "3 sets, 12, 12, 10 reps",
+                0,
+                Constants.ARMS) );
+
+        for(int i = 0; i < exercises.size(); i++){
+            values.put(EXERCISE_NAME, exercises.get(i).getExerciseName());
+            values.put(WEIGHT, exercises.get(i).getRecentWeight());
+            values.put(SETS_REPS, exercises.get(i).getSetsAndReps());
+            values.put(DATE, Constants.DEFAULT_DATE);
+            values.put(EXERCISE_TARGET, exercises.get(i).getExerciseTarget());
+            db.insert(TABLE_NAME, null, values);
+        }
+    } // end createDefaultCuttingTable
 }
